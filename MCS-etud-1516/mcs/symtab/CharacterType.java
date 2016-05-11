44cdf557bb045394b93d0a4b3670a3c2ccae43c9
Crabe05 <guillaume.dupont@etu.enseeiht.fr>
2016-05-11 15:12:09 +0200

    Added CharacterType class

---
+++ MCS-etud-1516/mcs/symtab/CharacterType.java
@@ -0,0 +1,31 @@
+/**
+ * CharacterType -- class representing the character type
+ *
+ * @author G. Dupont
+ * @version 0.1
+ */
+package mcs.symtab;
+
+class CharacterType implements Type {
+	/**
+	 * Constructor
+	 */
+	public CharacterType() {
+	}
+
+	/**
+	 * toString();
+	 */
+	public String toString() {
+		return "CHAR";
+	}
+
+	/**
+	 * isCompatible()
+	 */
+	public boolean isCompatible(Type other) {$
+		return (other instanceof IntegerType);
+	}
+}
+
+
