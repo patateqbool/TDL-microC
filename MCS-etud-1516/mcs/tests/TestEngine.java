1b9b6e47e0867d8c36278325ea26f7b1646b1769
Crabe05 <guillaume.dupont@etu.enseeiht.fr>
2016-05-12 14:02:56 +0200

    Added a 'tests' directory for making little tests of the engine

---
+++ MCS-etud-1516/mcs/tests/TestEngine.java
@@ -0,0 +1,21 @@
+import mcs.gc.*;
+import mcs.symtab.*;
+
+class TestEngine {
+
+	public static void main(String args[]) {
+		String code = "";
+		ARMEngine engine = new ARMEngine();
+		Register r1 = new Register("R", -1);
+		Register r2 = new Register("R", -1);
+		Register rout = new Register("R", -1);
+		code =
+			engine.generateLoadVariable(new VariableInfo(null, 26), r1) +
+			engine.generateLoadInteger(0x00123456, r2) +
+			engine.generateOperation(IMachine.Operator.ADD, r1, r2, rout);
+		System.out.println(code);
+		System.out.println("output register : " + rout);
+	}
+}
+
+
