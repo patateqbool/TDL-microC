cf21184b111065318d02bdb0f040a372e567020e
krab5 <crab.delicieux@gmail.com>
2016-05-25 13:49:37 +0200

    Added an exception raised whenevere there is no more register that can be used

---
+++ MCS-etud-1516/mcs/compiler/MCSRegisterLimitReachedException.java
@@ -0,0 +1,16 @@
+/**
+ * An exception for indicating that there is no more register available
+ *
+ * @author G. Dupont
+ * @version 0.1
+ */
+package mcs.compiler;
+
+class MCSRegisterLimitReachedException extends MCSException {
+  private static final long serialVersionUID = 1l;
+  public MCSRegisterLimitReachedException() {
+    super("No more free register to use !");
+  }
+}
+
+
