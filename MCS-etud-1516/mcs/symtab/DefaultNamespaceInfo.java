a258f0a3fb76ca02bd2c98e2bc950e45d82d1304
Crabe05 <guillaume.dupont@etu.enseeiht.fr>
2016-06-09 13:44:00 +0200

    Added a true default namespace

---
+++ MCS-etud-1516/mcs/symtab/DefaultNamespaceInfo.java
@@ -0,0 +1,14 @@
+/**
+ * Represent the default namespace
+ * @author G. Dupont
+ * @version 0.1
+ */
+package mcs.symtab;
+
+public class DefaultNamespaceInfo extends NamespaceInfo {
+	public DefaultNamespaceInfo() {
+		super("__default__");
+	}
+}
+
+
